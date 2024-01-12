3831962999c6413205368fbb0aafc111cc0b0bbc
haquocdat543 <wwwdatha543@gmail.com>
2024-01-12 10:04:31 +0000

    Update scripts

--- scripts/master.sh
+++ scripts/master.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
 hostnamectl set-hostname Hadoop-Master
+export HADOOP_VER="3.3.6"
+yum install java -y
+wget http://apache.cs.utah.edu/hadoop/common/current/hadoop-${HADOOP_VER}.tar.gz
+tar -xvzf hadoop-${HADOOP_VER}.tar.gz
+mv -f hadoop-${HADOOP_VER} hadoop
+rm -f hadoop-${HADOOP_VER}.tar.gz
--- scripts/worker3.sh
+++ scripts/worker3.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
 hostnamectl set-hostname Hadoop-Worker3
+yum install java -y
+wget http://apache.cs.utah.edu/hadoop/common/current/hadoop-${HADOOP_VER}.tar.gz
+tar -xvzf hadoop-${HADOOP_VER}.tar.gz
+mv -f hadoop-${HADOOP_VER} hadoop
+rm -f hadoop-${HADOOP_VER}.tar.gz
